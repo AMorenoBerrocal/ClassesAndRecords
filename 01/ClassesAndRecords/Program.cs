52b6a810391437b4d9bc0997278b88d4fe43c418
AMorenoBerrocal <164160063+AMorenoBerrocal@users.noreply.github.com>
2024-04-05 11:45:36 +0200

    Add files via upload

---
+++ 01/ClassesAndRecords/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace ClassesAndRecords
+{
+    class Program
+    {
+        static void PrintCourse(Course course)
+        {
+            Console.WriteLine(course.Name);
+        }
+
+        static void Main(string[] args)
+        {
+            var pluralsightCourse = new Course();
+            pluralsightCourse.Name = "Working with C# records";
+            pluralsightCourse.Author = "Alejandro";
+
+            var classroomCourse = new CourseRecord("Another title", "Alejandro");
+            // No necesitas asignar a la propiedad Name ya que estás usando registros
+
+            PrintCourse(classroomCourse); // Esta línea imprime "Another title"
+        }
+    }
+
+    public class Course
+    {
+        public string Name { get; set; }
+        public string Author { get; set; }
+    }
+
+    public record CourseRecord(string Name, string Author);
+}
