9385bffc4c1c63f5aa2e901be2d16b21beec9954
AVANADE-CORP\a.moreno.berrocal <a.moreno.berrocal@avanade.com>
2024-04-05 12:08:01 +0200

    modify program.cs

--- 02/Web/Program.cs
+++ 02/Web/Program.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Net.Http;
+using System.Reflection.Emit;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Microsoft.Extensions.DependencyInjection;
@@ -9,8 +10,16 @@ namespace Web
 {
     public class Program
     {
+
         public static async Task Main(string[] args)
         {
+            var pluralsightCourse = new Course();
+            pluralsightCourse.Name = "Working with c# records";
+            pluralsightCourse.Author = "Alejandro";
+            var (name, author) = pluralsightCourse;
+            Console.WriteLine(name);
+            Console.WriteLine(author);
+
             var builder = WebAssemblyHostBuilder.CreateDefault(args);
             builder.RootComponents.Add<App>("#app");
 
@@ -21,4 +30,18 @@ namespace Web
             await builder.Build().RunAsync();
         }
     }
+
+    public class Course
+    {
+        public string Name { get; set; }
+        public string Author { get; set; }
+
+        public void Deconstruct(out string name, out string author)
+        {
+            name = Name;
+            author = Author;
+        }
+
+    }
+    public record CourseRecord(string Name, String Description);
 }
