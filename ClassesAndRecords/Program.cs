ceefb673e0c48f42183f3cd296168b1a5814c00a
AVANADE-CORP\a.moreno.berrocal <a.moreno.berrocal@avanade.com>
2024-04-05 09:59:18 +0200

    Primer commit

---
+++ ClassesAndRecords/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassesAndRecords
+{
+    class Program
+    {
+
+        static void PrintCourse(Course course)
+        {
+            Console.WriteLine(course.Name);
+        }
+
+        static void Main(string[] args)
+        {
+            var pluralsightCourse = new Course();
+            pluralsightCourse.Name = "Working with c# records";
+            pluralsightCourse.Author = "Alejandro";
+
+            PrintCourse(pluralsightCourse);
+
+            var anotherCourse = pluralsightCourse;
+
+            Console.WriteLine(pluralsightCourse.Name);
+            Console.WriteLine(anotherCourse.Author);
+
+        }
+    }
+
+    public class Course
+    {
+        public string Name { get; set; }
+        public string Author { get; set; }
+    }
+}
